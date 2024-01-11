c530cce354366eccb2896e42e5260bf42f8fb9fe
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-01-12 00:18:08 +0100

    packer->lazy harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,24 @@
+return {
+    'theprimeagen/harpoon',
+    keys = {
+        { '<leader>ha', desc = 'Harpoon: add file' },
+        { '<leader>hm', desc = 'Harpoon: quick menu' }, 
+        { '<leader>h1', desc = 'Harpoon: file 1' },
+        { '<leader>h2', desc = 'Harpoon: file 2' },
+        { '<leader>h3', desc = 'Harpoon: file 3' },
+        { '<leader>h4', desc = 'Harpoon: file 4' },
+    },
+    config = function()
+        local mark = require('harpoon.mark')
+        local ui = require('harpoon.ui')
+
+        vim.keymap.set('n', '<leader>ha', mark.add_file)
+        vim.keymap.set('n', '<leader>hm', ui.toggle_quick_menu)
+
+        vim.keymap.set('n', '<leader>h1', function() ui.nav_file(1) end)
+        vim.keymap.set('n', '<leader>h2', function() ui.nav_file(2) end)
+        vim.keymap.set('n', '<leader>h3', function() ui.nav_file(3) end)
+        vim.keymap.set('n', '<leader>h4', function() ui.nav_file(4) end)
+    end
+}
+
