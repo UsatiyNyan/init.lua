ba4a7ef82f2cbad541d4db97e6a18af175e5f8d7
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-01-12 19:41:05 +0100

    localleader and clipboard

--- lua/usatiynyan/set.lua
+++ lua/usatiynyan/set.lua
@@ -16,6 +16,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
+vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -30,4 +31,5 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = '100'
 
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
