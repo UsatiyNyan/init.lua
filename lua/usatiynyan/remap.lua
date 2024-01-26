977fbea167bee9218d93f6d85a91db2517bcd106
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-01-26 23:56:43 +0100

    greates remaps ever

--- lua/usatiynyan/remap.lua
+++ lua/usatiynyan/remap.lua
@@ -6,3 +6,12 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Greatest remaps ever 
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move selected up' })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move selected down' })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste and dont lose the paste-buffer' })
+
+-- Greatest remap ever: AsbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = 'yank!' })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Yank!' })
+
