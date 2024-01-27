e5867cb41d0f011d8b9e3d309d1459f8aa340efc
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-01-27 03:51:13 +0100

    copilot.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,22 +8,22 @@ return {
         end,
         opts = {
             plugins = {
-                marks = true, -- shows a list of your marks on ' and `
+                marks = true,     -- shows a list of your marks on ' and `
                 registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
                 -- the presets plugin, adds help for a bunch of default keybindings in Neovim
                 -- No actual key bindings are created
                 spelling = {
-                    enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+                    enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
                     suggestions = 20, -- how many suggestions should be shown in the list?
                 },
                 presets = {
-                    operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-                    motions = true, -- adds help for motions
+                    operators = true,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
+                    motions = true,      -- adds help for motions
                     text_objects = true, -- help for text objects triggered after entering an operator
-                    windows = true, -- default bindings on <c-w>
-                    nav = true, -- misc bindings to work with windows
-                    z = true, -- bindings for folds, spelling and others prefixed with z
-                    g = true, -- bindings for prefixed with g
+                    windows = true,      -- default bindings on <c-w>
+                    nav = true,          -- misc bindings to work with windows
+                    z = true,            -- bindings for folds, spelling and others prefixed with z
+                    g = true,            -- bindings for prefixed with g
                 },
             },
             -- add operators that will trigger motion and text object completion
@@ -35,23 +35,40 @@ return {
                 group = '+', -- symbol prepended to a group
             },
             window = {
-                border = 'none', -- none, single, double, shadow
-                position = 'bottom', -- bottom, top
-                margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+                border = 'none',          -- none, single, double, shadow
+                position = 'bottom',      -- bottom, top
+                margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
                 padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
             },
         },
     },
     {
-        'mbbill/undotree', lazy = false, keys = {
+        'mbbill/undotree',
+        lazy = false,
+        keys = {
             { '<leader>u', vim.cmd.UndotreeToggle, desc = 'Undotree' },
         }
     },
     {
-        'tpope/vim-fugitive', lazy = false, keys = {
+        'tpope/vim-fugitive',
+        lazy = false,
+        keys = {
             { '<leader>gg', vim.cmd.Git, desc = 'Git Status' }
         }
     },
-    { 'github/copilot.vim', lazy = false }
+    {
+        'zbirenbaum/copilot.lua',
+        lazy = false,
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+        }
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        lazy = false,
+        config = function()
+            require("copilot_cmp").setup()
+        end,
+    }
 }
-
