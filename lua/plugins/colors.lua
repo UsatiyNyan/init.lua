3057a0ad5ff7d77b87609fc88a1c027135dc72ea
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-02-21 01:01:42 +0100

    oxocarbon theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -13,13 +13,23 @@ return {
     },
     {
         'navarasu/onedark.nvim',
-        lazy = false,
-        priority = 1000,
         config = function()
             require('onedark').setup({
                 style = 'deep'
             })
-            vim.cmd([[colorscheme onedark]])
+            -- vim.cmd.colorscheme 'onedark'
+        end
+    },
+    {
+        'nyoom-engineering/oxocarbon.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.opt.background = 'dark'
+            vim.cmd.colorscheme 'oxocarbon'
+            -- for transparent background:
+            -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+            -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
         end
     }
 }
