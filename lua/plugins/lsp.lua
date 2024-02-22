8a7a3c61eb59f6f8f76c946f2eba467b04ec098a
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-02-22 01:02:20 +0100

    fix signature documentation working with vim-tmux

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,6 +12,7 @@ local on_attach = function(_, bufnr)
     nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
     nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
     nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
@@ -20,10 +21,9 @@ local on_attach = function(_, bufnr)
 
     -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    nmap('<leader>dp', vim.lsp.buf.signature_help, '[D]ocument [P]arameters')
 
     -- Lesser used LSP functionality
-    -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
     -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
     -- nmap('<leader>wl', function()
