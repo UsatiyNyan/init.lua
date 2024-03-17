b666ce8276a7f06e7d1fa62d23c6a8166f6e20e2
UsatiyNyan <us4tiyny4n@gmail.com>
2024-03-17 16:10:02 +0100

    cmp insert

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -43,7 +43,7 @@ return {
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-y>'] = cmp.mapping.confirm {
-                        behavior = cmp.ConfirmBehavior.Replace,
+                        behavior = cmp.ConfirmBehavior.Insert,
                         select = true,
                     },
                 },
