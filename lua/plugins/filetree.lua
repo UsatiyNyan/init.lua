6ce05abb95d45268e3ef72f6a1cebcd4ee1ce58f
UsatiyNyan <us4tiyny4n@gmail.com>
2024-06-13 20:40:27 +0200

    filtree returns

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,18 +1,31 @@
 return {
-    'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('oil').setup {
-            columns = { 'icon' },
-            keymaps = {
-                ['<C-h>'] = false,
-                ['<M-h>'] = 'actions.select_split',
-            },
-            view_options = {
-                show_hidden = true,
-            },
-        }
-        vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-        vim.keymap.set('n', '<leader>-', require('oil').toggle_float)
-    end,
+    {
+        'stevearc/oil.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('oil').setup {
+                columns = { 'icon' },
+                keymaps = {
+                    ['<C-h>'] = false,
+                },
+                view_options = {
+                    show_hidden = true,
+                },
+            }
+            vim.keymap.set('n', '<leader>-', require('oil').toggle_float)
+        end,
+    },
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'MunifTanjim/nui.nvim',
+            '3rd/image.nvim', -- Optional image support in preview window
+        },
+        keys = {
+            { '<leader>nt', '<cmd>Neotree toggle<cr>', desc = 'NeoTree' },
+        },
+        config = true,
+    },
 }
