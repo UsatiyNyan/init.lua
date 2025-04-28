69e86d412457df2439c9caa61b1bf38dfaa74682
UsatiyNyan <us4tiyny4n@gmail.com>
2025-04-28 22:02:26 +0200

    colors: fixes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -23,7 +23,7 @@ return {
     {
         'nyoom-engineering/oxocarbon.nvim',
         config = function()
-            -- vim.opt.background = 'light'
+            vim.opt.background = 'dark'
             -- vim.cmd.colorscheme 'oxocarbon'
             -- for transparent background:
             -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
@@ -35,8 +35,7 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            vim.g.rose_pine_variant = 'moon'
-            vim.opt.background = 'dark'
+            vim.g.rose_pine_variant = 'main'
             vim.cmd.colorscheme 'rose-pine'
         end
     },
